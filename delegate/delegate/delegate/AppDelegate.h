c8bd2f7acd04635840ba15474882a0edaf9010ff
heidanchaofanhei <shenglin@ShenglindeMacBook-Pro.local>
2016-03-10 16:06:57 +0800

    新建

---
+++ delegate/delegate/delegate/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  delegate
+//
+//  Created by Shenglin on 16/3/10.
+//  Copyright © 2016年 Shenglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ delegate/delegate/delegate/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  delegate
+//
+//  Created by Shenglin on 16/3/10.
+//  Copyright © 2016年 Shenglin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
